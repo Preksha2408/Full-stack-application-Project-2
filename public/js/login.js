e1abb2072dfe10ca1e27377e15fe9668c35ad5e9
Preksha Sangvikar <prekshasangvikar@Prekshas-MacBook-Pro.local>
2024-08-09 17:54:02 -0700

    Modified login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,11 @@
 document.querySelector("#login-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const loginObj = {
-    email: document.querySelector("#username").value,
+    username: document.querySelector("#username").value,
     password: document.querySelector("#password").value,
   };
 
+  console.log("Here is the loginObj",loginObj);
   fetch("/api/users/login", {
     method: "POST",
     body: JSON.stringify(loginObj),
@@ -15,7 +16,7 @@ document.querySelector("#login-form").addEventListener("submit", (e) => {
     console.log("Response for login-form",res);
     if (res.ok) {
       console.log("Login successful, redirecting to homepage");
-      location.href = "/homepage";
+      location.reload ();
     } else {
       res.json().then(data => console.log(data));
       alert("login failed. Please Try again");
